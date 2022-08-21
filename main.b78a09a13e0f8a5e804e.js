9d57954876f7592365b483664da44d67e8ad9857
Azzaid <Johanas.Azzaid@gmail.com>
2022-08-21 20:56:22 +0200

    Updates

--- main.b78a09a13e0f8a5e804e.js
+++ main.b78a09a13e0f8a5e804e.js
@@ -5551,7 +5551,7 @@ function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 var StyledLoginHolder = styled_components__WEBPACK_IMPORTED_MODULE_6__["default"].div.withConfig({
   displayName: "Login__StyledLoginHolder",
   componentId: "sc-kkqmm5-0"
-})(["width:100%;height:100%;display:flex;align-items:center;justify-content:center;.button-65{appearance:none;backface-visibility:hidden;background-color:#2f80ed;border-radius:10px;border-style:none;box-shadow:none;box-sizing:border-box;color:#fff;cursor:pointer;display:inline-block;font-family:Inter,-apple-system,system-ui,\"Segoe UI\",Helvetica,Arial,sans-serif;font-size:15px;font-weight:500;height:50px;letter-spacing:normal;line-height:1.5;outline:none;overflow:hidden;padding:14px 30px;position:relative;text-align:center;text-decoration:none;transform:translate3d(0,0,0);transition:all .3s;user-select:none;-webkit-user-select:none;touch-action:manipulation;vertical-align:top;white-space:nowrap;}.button-65:hover{background-color:#1366d6;box-shadow:rgba(0,0,0,.05) 0 5px 30px,rgba(0,0,0,.05) 0 1px 4px;opacity:1;transform:translateY(0);transition-duration:.35s;}.button-65:hover:after{opacity:.5;}.button-65:active{box-shadow:rgba(0,0,0,.1) 0 3px 6px 0,rgba(0,0,0,.1) 0 0 10px 0,rgba(0,0,0,.1) 0 1px 4px -1px;transform:translateY(2px);transition-duration:.35s;}.button-65:active:after{opacity:1;}@media (min-width:768px){.button-65{padding:14px 22px;width:176px;}}"]);
+})(["width:100%;height:100%;display:flex;align-items:center;justify-content:center;flex-direction:column;.button-65{appearance:none;backface-visibility:hidden;background-color:#2f80ed;border-radius:10px;border-style:none;box-shadow:none;box-sizing:border-box;color:#fff;cursor:pointer;display:inline-block;font-family:Inter,-apple-system,system-ui,\"Segoe UI\",Helvetica,Arial,sans-serif;font-size:15px;font-weight:500;height:50px;letter-spacing:normal;line-height:1.5;outline:none;overflow:hidden;padding:14px 30px;position:relative;text-align:center;text-decoration:none;transform:translate3d(0,0,0);transition:all .3s;user-select:none;-webkit-user-select:none;touch-action:manipulation;vertical-align:top;white-space:nowrap;}.button-65:hover{background-color:#1366d6;box-shadow:rgba(0,0,0,.05) 0 5px 30px,rgba(0,0,0,.05) 0 1px 4px;opacity:1;transform:translateY(0);transition-duration:.35s;}.button-65:hover:after{opacity:.5;}.button-65:active{box-shadow:rgba(0,0,0,.1) 0 3px 6px 0,rgba(0,0,0,.1) 0 0 10px 0,rgba(0,0,0,.1) 0 1px 4px -1px;transform:translateY(2px);transition-duration:.35s;}.button-65:active:after{opacity:1;}@media (min-width:768px){.button-65{padding:14px 22px;width:176px;}}"]);
 _c = StyledLoginHolder;
 
 var Login = function Login(props) {
@@ -50964,4 +50964,4 @@ function _objectWithoutPropertiesLoose(source, excluded) {
 /******/ 	
 /******/ })()
 ;
-//# sourceMappingURL=main.a24609ba0ba316c491f7.js.map
+//# sourceMappingURL=main.b78a09a13e0f8a5e804e.js.map
